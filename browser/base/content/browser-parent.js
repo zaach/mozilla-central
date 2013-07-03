74a1ef4d3d19d218cc87d08c60688fcc7110c505
Bill McCloskey <wmccloskey@mozilla.com>
2013-07-03 13:44:49 -0700

    Remove shouldLoad printing.

--- browser/base/content/browser-parent.js
+++ browser/base/content/browser-parent.js
@@ -32,7 +32,6 @@ let AddonParent = {
   },
 
   shouldLoad: function(target, json, remote) {
-    dump('--------------------------------- HOOK ShouldLoad ---\n');
     var contentLocation = remote.contentLocation;
     var requestOrigin = remote.requestOrigin;
     var node = remote.node;
@@ -44,7 +43,6 @@ let AddonParent = {
     while (list.hasMoreElements()) {
       var item = list.getNext();
       var service = item.QueryInterface(Components.interfaces.nsISupportsCString).toString();
-      dump("!!! SERVICE: " + service + "\n");
       if (!(service in Cc))
         continue;
       var policy = Cc[service].getService(Ci.nsIContentPolicy);
@@ -61,12 +59,10 @@ let AddonParent = {
           throw e;
       }
       if (r != Ci.nsIContentPolicy.ACCEPT && r != 0) {
-        dump("@@@@@@ service \"" + service + "\" rval: " + r + "\n");
         return r;
       }
     }
 
-    dump('--------------------------------- ENDHOOK ShouldLoad ---\n');
     return Ci.nsIContentPolicy.ACCEPT;
   }
 };
