9951e2bf830f3f81daca361062d61da23c2ba2b5
Mark Hammond <mhammond@skippinet.com.au>
2013-09-20 17:13:24 +1000

    Disable tests which fail in e10s due to bug 916974 (Session History)

--- browser/base/content/test/general/browser_bug647886.js
+++ browser/base/content/test/general/browser_bug647886.js
@@ -1,5 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+e10sSkip("Bug 916974 - Session history doesn't work in e10s");
 
 function test() {
   waitForExplicitFinish();
