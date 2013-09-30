cd582256afc35d7698afb52f853d3a2ffa527cb0
Mark Hammond <mhammond@skippinet.com.au>
2013-09-30 15:43:34 +1000

    Disable tests which fail in e10s due to bug 921916 (no plugin events)

--- browser/base/content/test/general/browser_bug797677.js
+++ browser/base/content/test/general/browser_bug797677.js
@@ -2,6 +2,8 @@
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+e10sSkip("Bug 921916 - no plugin events");
+
 var rootDir = getRootDirectory(gTestPath);
 const gHttpTestRoot = rootDir.replace("chrome://mochitests/content/", "http://127.0.0.1:8888/");
 const Cc = Components.classes;
