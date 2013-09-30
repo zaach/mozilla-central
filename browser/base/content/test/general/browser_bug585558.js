7989a84737fc38ecc68c234ba2ba5cb9cc69c6ad
Mark Hammond <mhammond@skippinet.com.au>
2013-09-30 11:33:06 +1000

    Disable tests which fail in e10s due to bug 921905 (pinTab/unpinTab)

--- browser/base/content/test/general/browser_bug585558.js
+++ browser/base/content/test/general/browser_bug585558.js
@@ -1,6 +1,7 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
+e10sSkip("Bug 921905 - pinTab/unpinTab fail in e10s");
 
 let tabs = [];
 
--- browser/base/content/test/general/browser_visibleTabs_bookmarkAllTabs.js
+++ browser/base/content/test/general/browser_visibleTabs_bookmarkAllTabs.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+e10sSkip("Bug 921905 - pinTab/unpinTab fail in e10s");
+
 function test() {
   waitForExplicitFinish();
 
