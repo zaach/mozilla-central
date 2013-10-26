079971538a129e50f509a3e5b7a4da77312ee3db
Brian Warner <warner@lothar.com>
2013-10-25 17:59:32 -0700

    rearrange promise structure, add basic test

--- services/fxaccounts/tests/xpcshell/test_accounts.js
+++ services/fxaccounts/tests/xpcshell/test_accounts.js
@@ -5,8 +5,10 @@
 
 const {interfaces: Ci, results: Cr, utils: Cu} = Components;
 
+Cu.import("resource://services-common/utils.js");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/FxAccounts.jsm");
+Cu.import("resource://gre/modules/Promise.jsm");
 
 function run_test() {
   run_next_test();
@@ -34,6 +36,7 @@ add_test(function test_non_https_remote_server_uri() {
   run_next_test();
 });
 
+/*
 add_task(function test_get_signed_in_user_initially_unset() {
   // user is initially undefined
   let result = yield fxAccounts.getSignedInUser();
@@ -65,6 +68,7 @@ add_task(function test_get_signed_in_user_initially_unset() {
 
   run_next_test();
 });
+*/
 
 add_test(function test_hawk_credentials() {
   let sessionToken = "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf";
@@ -73,5 +77,60 @@ add_test(function test_hawk_credentials() {
   do_check_eq(result.id, "639503a218ffbb62983e9628be5cd64a0438d0ae81b2b9dadeb900a83470bc6b");
   do_check_eq(result.key, "3a0188943837ab228fe74e759566d0e4837cbcc7494157aac4da82025b2811b2");
 
+  run_next_test();
+});
+
+let _MockFXA = function() {
+  FxAccounts.apply(this, arguments);
+  this._d_fetchKeys = Promise.defer();
+};
+_MockFXA.prototype = {
+  __proto__: FxAccounts.prototype,
+  _checkEmailStatus: function(sessionToken) {
+    dump("== _checkEmailStatus\n");
+    return Promise.resolve({verified: true});
+  },
+  _fetchKeys: function(keyFetchToken) {
+    dump("== _fetchKeys\n");
+    return this._d_fetchKeys.promise;
+  },
+};
+
+add_task(function test_verification_poll() {
+  dump("----- START ----\n");
+  let a = new _MockFXA();
+  let creds = {
+    sessionToken: "sessionToken", keyFetchToken: "keyFetchToken",
+    unwrapBKey: "4444444444444444444444444444444444444444444444444444444444444444",
+  };
+  a.setSignedInUser(creds);
+  let data = yield a._getUserAccountData();
+  do_check_eq(a._isReady(data), false);
+  data = yield a.getSignedInUser();
+  do_check_eq(data, null);
+  yield a._whenVerified();
+  data = yield a._getUserAccountData();
+  do_check_eq(a._isReady(data), false);
+  do_check_eq(data.isVerified, true);
+
+  a._d_fetchKeys.resolve({
+    kA: CommonUtils.hexToBytes("1111111111111111111111111111111111111111111111111111111111111111"),
+    wrapKB: CommonUtils.hexToBytes("2222222222222222222222222222222222222222222222222222222222222222"),
+  });
+
+  yield a._whenReady();
+  dump("== now ready in throery\n");
+  data = yield a._getUserAccountData();
+  do_check_eq(a._isReady(data), true);
+  do_check_eq(data.kA, "1111111111111111111111111111111111111111111111111111111111111111");
+  do_check_eq(data.kB, "6666666666666666666666666666666666666666666666666666666666666666");
+  do_check_eq(data.keyFetchToken, undefined);
+
+  data = yield a.getSignedInUser();
+  do_check_eq(data.kA, "1111111111111111111111111111111111111111111111111111111111111111");
+  do_check_eq(data.kB, "6666666666666666666666666666666666666666666666666666666666666666");
+  do_check_eq(data.keyFetchToken, undefined);
+
+  dump("----- DONE ----\n");
   run_next_test();
 });
